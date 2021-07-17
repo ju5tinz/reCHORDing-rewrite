88ae6b40af4c373b693b2afe425791be3339d2bb
Justin Zhang <ju5tinz.mlearn@gmail.com>
2021-07-17 01:34:58 -0700

    Tailwind init

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import { Provider } from 'react-redux';
 import store from './state/store';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import Header from './components/Header'
+
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div className="App">
-          
+        <div className="flex flex-col min-h-screen">
+          <Header />
         </div>
       </Router>
     </Provider>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div className="fixed w-full">
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
